0328c18389fc8319e51807471b3752b173af8e46
afcrossland <andrew@example.com>
2026-06-10 16:06:12 +0100

    added carbon tracker

--- assets/nav.js
+++ assets/nav.js
@@ -15,6 +15,7 @@ const NAV_HTML = `
             <li><a href="/last-28-days/index.html">Last 28 Days</a></li>
             <li><a href="/last-12-months/index.html">Last 12 Months</a></li>
             <li><a href="/historicaldata/index.html">Generation History</a></li>
+            <li><a href="/carbon-tracker/index.html">Carbon Tracker</a></li>
           </ul>
         </li>
         <li><a href="/2030grid/index.html">The 2030 Grid</a></li>
